2ae82f6f57a3b8a4ff9dfba522baabd18b3cf2af
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2019-09-10 10:14:14 +0100

    Feat(Nav): Add Navbar Toggle For Mobile And Tablet Screens

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,47 +1,59 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./header.module.scss"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "gatsby"
 import cx from "classnames"
 
-export default ({ type }) => (
-  <nav className={cx(styles.nav, { section: true, [`${type}-bg`]: true })}>
-    <div className={styles.left}>
-      <Link to="/" className={styles.brand}>
-        <h2>
-          <span role="img" aria-label="telescope">
-            🔭
-          </span>
-          &nbsp;Rust Telescope
-        </h2>
-      </Link>
-    </div>
+export default ({ type }) => {
+  const [toggle, switchToggle] = useState(false)
 
-    <div className={styles.middle}></div>
+  return (
+    <nav className={cx(styles.nav, { section: true, [`${type}-bg`]: true })}>
+      <div className={styles.left}>
+        <Link to="/" className={styles.brand}>
+          <h2>
+            <span role="img" aria-label="telescope">
+              🔭
+            </span>
+            &nbsp;Rust Telescope
+          </h2>
+        </Link>
 
-    <div className={styles.right}>
-      <Link to="/interviews" className={styles.link}>
-        Interviews
-      </Link>
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={() => switchToggle(!toggle)}
+          className={cx(styles.navToggle)}
+        />
+      </div>
 
-      <Link to="/rust-a-little" className={styles.link}>
-        Rust a Little
-      </Link>
+      <div className={styles.middle}></div>
 
-      <Link to="/hacking-with-rust" className={styles.link}>
-        Hacking With Rust
-      </Link>
+      <div className={cx(styles.right, toggle === true ? styles.toggled : "")}>
+        <Link to="/interviews" className={styles.link}>
+          Interviews
+        </Link>
 
-      <Link to="/blog" className={styles.link}>
-        Blog
-      </Link>
+        <Link to="/rust-a-little" className={styles.link}>
+          Rust a Little
+        </Link>
 
-      <a
-        target="_"
-        href="https://www.patreon.com/rust_telescope"
-        className={cx(styles.link, styles.patreon)}
-      >
-        Sponsor Us
-      </a>
-    </div>
-  </nav>
-)
+        <Link to="/hacking-with-rust" className={styles.link}>
+          Hacking With Rust
+        </Link>
+
+        <Link to="/blog" className={styles.link}>
+          Blog
+        </Link>
+
+        <a
+          target="_"
+          href="https://www.patreon.com/rust_telescope"
+          className={cx(styles.link, styles.patreon)}
+        >
+          Sponsor Us
+        </a>
+      </div>
+    </nav>
+  )
+}
